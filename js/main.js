56bcd41044c6218760345383647d08307d5f8812
Ariel Barclay <mrsbarclay101008@gmail.com>
2024-12-13 10:42:50 -0500

    Made changes to the calculator pad and buttons. Sudo code for js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
 console.log("Working");
+//When number or operator li is clicked combine them into a mathmatical expression (able to make a long expression not just two numbers and an operator)
+//display the mathmatical expression in the display div
+// when enter is clicked display the result of the expression
